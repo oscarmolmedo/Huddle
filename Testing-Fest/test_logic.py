54131a36f1b84ddcafe2583d056a3fb2387fecfc
oscarmolmedo <oscaaraujo95@gmail.com>
2026-03-11 16:17:18 -0300

    feat:  Añade test para validacion de mensajes y verificacion longitud de nickname

--- Testing-Fest/test_logic.py
+++ Testing-Fest/test_logic.py
@@ -1,24 +1,45 @@
-from logic import procesar_protocolo, validar_nick, limpiar_mensaje
+from logic import procesar_protocolo, validar_nick, limpiar_mensaje, es_mensaje_valido
 import pytest
 
-# PRUEBA 1: Caso Positivo - ¿Funciona el protocolo MSG?
+# --- LOGICA DE FORMATO CORRECTO ---
+# Caso Positivo
 def test_protocolo_mensaje_valido():
     tipo, contenido = procesar_protocolo("MSG|hola servidor")
     assert tipo == "MSG"
     assert contenido == "hola servidor"
 
-# PRUEBA 2: Caso Negativo - ¿Qué pasa si envío algo sin el pipe '|'?
+# Caso Negativo
 def test_protocolo_sin_separador():
     with pytest.raises(ValueError): # Esperamos que falle con esta excepción
         procesar_protocolo("MENSAJE_INVALIDO")
 
-# PRUEBA 3: Validación de Nick
-def test_nick_demasiado_corto():
-    assert validar_nick("ab") is False
 
+# --- LOGICA DE NICKS ---
+# Caso Positivo
 def test_nick_valido():
     assert validar_nick("python_dev") is True
 
-# PRUEBA 4: Limpia espacios del mensaje
+# Caso Negativo
+def test_nick_demasiado_corto():
+    assert validar_nick("ab") is False
+
+
+# --- LOGICA DE LIMPIAR MENSAJE ---
+# Caso Positivo
 def test_mensaje_limpio():
     assert limpiar_mensaje("   Holaaa, como estas.   ") == 'Holaaa, como estas.'
+
+# Caso Negativo (Error de Tipo)
+def test_mensaje_limpio_tipo_incorrecto():
+    assert limpiar_mensaje(3232322) is False# Los números no tienen el método .strip()
+
+
+# --- LOGICA DE LIMPIAR MENSAJE ---
+# Caso Positivo
+    mensaje = "Aa" * 50
+    assert es_mensaje_valido(mensaje) is True
+
+# Caso Negativo
+def test_mensaje_texto_valido():
+    mensaje = "Aa" * 101
+    assert es_mensaje_valido(mensaje) is False
