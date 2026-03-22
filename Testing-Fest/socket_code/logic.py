cc8c3e0c2b68499951ecf5fd2c6ec7bfd0ef0c17
oscarmolmedo <oscaaraujo95@gmail.com>
2026-03-22 20:52:15 -0300

    refactor: Elimina funciones innecesarias y simplifica la validación de mensajes en logic.py

--- Testing-Fest/socket_code/logic.py
+++ Testing-Fest/socket_code/logic.py
@@ -13,36 +13,14 @@ def procesar_protocolo(data_decodificada):
 
 def validar_nick(nombre):
     """
-    Retorna True si el nick es válido (3 o más caracteres), 
-    False de lo contrario.
+    Retorna True o False
     """
     return len(nombre.strip()) >= 3
-
-def limpiar_mensaje(mensaje):
-    """
-    Limpia todos los espacios al principio y final
-    de la cadena
-    """
-    if isinstance(mensaje, str):
-        return mensaje.strip()
-    else:
-        return False
 
 def es_mensaje_valido(mensaje):
     """
     .strip() si devuelve algo es True. Si queda "" devuelve False.
     Si mensaje es valido verifica longitud de cadena
     """
-    #OPCIÓN LARGA
-    # es_valido = bool(mensaje.strip())
-
-    # if es_valido:
-    #     if len(mensaje) > 100:
-    #         return False
-    #     else:
-    #         return True
-    # else:
-    #     return es_valido
-
     #OPCIÓN COMPACTA
     return bool(mensaje.strip() and len(mensaje) <= 100)
