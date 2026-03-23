7e869ceeabf054dd389d5884e42ca358eb288b66
oscarmolmedo <oscaaraujo95@gmail.com>
2026-03-22 21:01:07 -0300

    test: Añade pruebas unitarias e integración para la lógica del servidor

---
+++ Testing-Fest/test_final.py
@@ -0,0 +1,54 @@
+import pytest
+from unittest.mock import MagicMock
+from socket_code.logic import procesar_protocolo, es_mensaje_valido, validar_nick
+import socket_code.servidor as servidor
+
+
+
+# --- PRUEBAS UNITARIAS (Funciones Críticas) ---
+def test_unit_protocolo_invalido():
+    with pytest.raises(ValueError):
+        procesar_protocolo("mensaje_sin_pipe")
+
+# VALIDAR NICK
+def test_unit_nick_valido():
+    assert validar_nick("olos") is True
+    
+# --- PRUEBA TDD (Validación de longitud y contenido) ---
+def test_tdd_mensaje_vacio_y_largo():
+    assert es_mensaje_valido("   ") is False
+    assert es_mensaje_valido("A" * 101) is False
+
+#--- PRUEBAS DE INTEGRACIÓN (Múltiples clientes y Broadcast) ---
+def test_integracion_broadcast_exitoso():
+    clientes = []
+    servidor.nicks = {}
+    
+    c1, c2, c3 = MagicMock(), MagicMock(), MagicMock()
+    servidor.clientes.extend([c1, c2, c3])
+    servidor.nicks[c1], servidor.nicks[c2], servidor.nicks[c3] = "Goku", "Vegeta", "Picolo"
+    
+    servidor.enviar_a_todos(c1, "Hola")
+    
+    # C2 debe recibirlo, C1 no (no se envía a sí mismo)
+    c2.sendall.assert_called()
+    c3.sendall.assert_called()
+    c1.sendall.assert_not_called()
+
+# --- PRUEBA DE DESCONEXIÓN (Condición excepcional) ---
+def test_integracion_desconexion_abrupta():
+    
+    emisor  =  MagicMock()
+    victima =  MagicMock()
+    
+    # Forzamos que la víctima falle al recibir
+    victima.sendall.side_effect = Exception("Crash")
+    
+    servidor.clientes.extend([emisor, victima])
+    servidor.nicks[emisor], servidor.nicks[victima] = "E", "V"
+    
+    # El servidor no debe romperse al ejecutar esto
+    servidor.enviar_a_todos(emisor, "Test")
+    
+    # Verificamos que el servidor manejó el error eliminando al cliente
+    assert victima not in servidor.clientes
